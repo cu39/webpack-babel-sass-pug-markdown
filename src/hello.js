4eb7828afed12be13fbcf0311b3b3b90529c1297
Shusaku NAKAZATO <cu393uc@gmail.com>
2018-12-19 23:30:43 +0900

    Create Hello class

---
+++ src/hello.js
@@ -0,0 +1,11 @@
+class Hello {
+  constructor(msg) {
+    this.msg = msg
+  }
+
+  greet() {
+    return `Hello ${this.msg}!`
+  }
+}
+
+export default Hello
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-const msg = 'Hello!'
+import { default as Hello } from './hello'
+
+const hello = new Hello('John')
+const msg = hello.greet()
 console.log(msg)
 document.querySelector('#output').innerText = msg
